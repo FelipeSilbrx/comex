5370e98448346c76422aaf81fc2886468368eca7
FelipeSilbrx <felipe.silveira@brxretail.com>
2022-09-28 10:56:03 -0300

    Salvando os primeiros arquivos da semana 7

--- src/comex/Categoria.java
+++ src/comex/Categoria.java
@@ -4,7 +4,8 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 public class Categoria {
 	
-	private static final AtomicInteger count = new AtomicInteger(0); 
+	
+	private static final AtomicInteger count = new AtomicInteger(0);
 	private int id;;
 	private String nome;
 	private StatusCategoria status = StatusCategoria.ATIVA;
@@ -21,11 +22,23 @@ public class Categoria {
 		return status;
 	}
 
+	 void ValidarConstrutor() {
+		if (this.getId() <= 0 || this.getNome().length() <= 3 ) {
+			 throw new IllegalArgumentException();
+		}
+		else{
+			System.out.println("Construtor Validado");
+		}
+	}
+
 	public Categoria(String nome, StatusCategoria ativa) {
-		super();
+		super();		
 		this.id = count.incrementAndGet();
 		this.nome = nome;
 		this.status = ativa;
+
 	}
+	
+	
 
 }
--- src/comex/MainCategoria.java
+++ src/comex/MainCategoria.java
@@ -3,14 +3,25 @@ package comex;
 public class MainCategoria {
 	 public static void main (String[] args) {
 		
-		Categoria informatica = new Categoria("INFORMÁTICA",StatusCategoria.ATIVA);
-		Categoria moveis = new Categoria("MÓVEIS",StatusCategoria.INATIVA);
-		Categoria livros = new Categoria("LIVROS",StatusCategoria.ATIVA);		
+		Categoria informatica = new Categoria("INFORMATICA",StatusCategoria.ATIVA);
+		Categoria moveis = new Categoria("M",StatusCategoria.INATIVA);
+		Categoria livros = new Categoria("LIVROS",StatusCategoria.ATIVA);
 		
+		try {			
+			moveis.ValidarConstrutor();
+			livros.ValidarConstrutor();
+			
+		}catch(IllegalArgumentException IAex){
+			String msg = IAex.getMessage();
+			System.out.println("IllegalArgumentException " + msg);
+		}
+		/*
 		System.out.println("Categoria " + informatica.getNome() + " ("+informatica.getId() +" - "+informatica.getStatus() +")");
 		System.out.println("Categoria " + moveis.getNome() + " ("+moveis.getId() +" - "+moveis.getStatus() +")");
 		System.out.println("Categoria " + livros.getNome() + " ("+livros.getId() +" - "+livros.getStatus() +")");
-		
+		*/
 		
 	 }
+	 
+	 
 }
