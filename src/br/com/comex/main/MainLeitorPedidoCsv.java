0d006237ab459b2ef90bcfabb70f887328720e3e
FelipeSilbrx <felipe.silveira@brxretail.com>
2022-10-06 16:25:43 -0300

    Finalizando card de Montante total de vendas.

--- src/br/com/comex/main/MainLeitorPedidoCsv.java
+++ src/br/com/comex/main/MainLeitorPedidoCsv.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import br.com.comex.csv.LeitorPedidosCsv;
 import br.com.comex.csv.PedidoCsv;
+import br.com.comex.modelo.MontanteTotalVendas;
 import br.com.comex.modelo.TotalCategorias;
 import br.com.comex.modelo.TotalProdutosVendidos;
 
@@ -16,10 +17,12 @@ public class MainLeitorPedidoCsv {
 		
 		TotalProdutosVendidos tpv = new TotalProdutosVendidos();
 		TotalCategorias tc = new TotalCategorias();
+		MontanteTotalVendas mtv = new MontanteTotalVendas();		
 		
 		System.out.println("Total de pedidos: "+pedidosCsv.size());
 		System.out.println("Total de produtos vendidos: "+tpv.calcularProdutos(pedidosCsv));
 		System.out.println("Total de categorias: "+tc.calcularCategorias(pedidosCsv));
+		System.out.println("Montante de vendas: " + mtv.calcularMontante(pedidosCsv));
 		
 	}
 
